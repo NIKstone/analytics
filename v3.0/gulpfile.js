a0af14274a6ff27f67f0e6b95135ef4a9b86b9da
陈磊 <chenlei@2xi.com>
2017-12-15 15:06:49 +0800

    [stone]test report error

--- v3.0/gulpfile.js
+++ v3.0/gulpfile.js
@@ -1,28 +1,29 @@
 var gulp = require('gulp'),
-	clean = require('gulp-clean'),
-	jshint = require('gulp-jshint'),
-	uglify = require('gulp-uglify'),
-	rename = require('gulp-rename');
-	replace = require('gulp-replace');
+    clean = require('gulp-clean'),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename');
+replace = require('gulp-replace');
 
-	gulp.task("clean", function(){
-		return gulp.src(['bin'])
-				.pipe(clean());
-	});
+gulp.task("clean", function() {
+    return gulp.src(['bin'])
+        .pipe(clean());
+});
 
-	gulp.task("script", function(){
-		gulp.src(["lib/**/*.js"])
-			.pipe(jshint())
-			.pipe(uglify({
-	            mangle: {
-	                except: ['require']
-	            }
-	        }))
-	        .pipe(replace('51xiyou', '52xiyou'))
-			.pipe(rename({ suffix: ".min"}))
-			.pipe(gulp.dest("bin"))
-	});
+gulp.task("script", function() {
+    gulp.src(["lib/**/*.js"])
+        .pipe(jshint())
+        .pipe(uglify({
+            mangle: {
+                except: ['require']
+            }
+        }))
+        .pipe(replace('51xiyou', '52xiyou'))
+        .pipe(replace('xiyouence_test.com', 'xiyouence.com'))
+        .pipe(rename({ suffix: ".min" }))
+        .pipe(gulp.dest("bin"))
+});
 
-	gulp.task("default", ['clean'], function(){
-		gulp.start("script");
-	});
+gulp.task("default", ['clean'], function() {
+    gulp.start("script");
+});
